41944ab41346e89cf2ccc08ba309a6ec7c59442b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-07 14:46:12 +0430

    some migrations

--- users/models.py
+++ users/models.py
@@ -26,7 +26,7 @@ class InfluencerManager(BaseUserManager):
 
 # Create your models here.
 class Topic(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.title
@@ -71,7 +71,7 @@ class Influencer(User):
     is_general_page = models.BooleanField()
     card_number = models.CharField(max_length=16, null=True)
     account_number = models.CharField(max_length=26, null=True)
-    topics = models.ManyToManyField(Topic)
+    topics = models.ManyToManyField(Topic, related_name='Influencer_Topics')
 
     USERNAME_FIELD = 'email'
     objects = InfluencerManager()
