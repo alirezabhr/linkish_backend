a44656224332d204628e6e1ff5eab8b861a2d11f
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-08 20:26:08 +0430

    2 functions added to generate a unique short link

---
+++ ad/utils.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+
+def generate_random_string(size=6, chars=string.ascii_letters + string.digits):
+    new_str = ''
+    for _ in range(size):
+        new_str += random.choice(chars)
+    return new_str
+
+
+def get_random_link(instance, size=6):
+    new_str = generate_random_string(size=size)
+
+    qs_exists = instance.objects.filter(short_link=new_str).exists()
+    if qs_exists:
+        return get_random_link(instance=instance, size=size)
+
+    return new_str
