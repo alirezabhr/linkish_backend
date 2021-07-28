0a31f630c69ea43ba85b85b973512ec1a606578f
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-28 13:28:39 +0430

    email setting added in settings.py

--- linkish/settings.py
+++ linkish/settings.py
@@ -151,3 +151,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mehdi.ali.project@gmail.com'
+EMAIL_HOST_PASSWORD = 'mehdiali1400'
+EMAIL_USE_TLS = True
