2778af0459dc1839b73f49d1583ac6e524f5d6db
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-08-01 20:55:14 +0430

    AdViewerDetail model added in admin panel

--- ad/admin.py
+++ ad/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Ad, InfAd, SuggestAd
+from .models import Ad, InfAd, SuggestAd, AdViewerDetail
 
 # Register your models here.
 admin.site.register(Ad)
 admin.site.register(SuggestAd)
 admin.site.register(InfAd)
+admin.site.register(AdViewerDetail)
