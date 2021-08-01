439f7d3bdc99d367f83cdfe223297f121a3f3689
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-08-01 19:51:35 +0430

    get and check ip functions added in utils

--- ad/utils.py
+++ ad/utils.py
@@ -1,7 +1,7 @@
 import random
 import string
-import datetime
 import django.utils.timezone as django_tz
+from geoip2.webservice import Client
 
 
 def generate_random_string(size=6, chars=string.ascii_letters + string.digits):
@@ -25,5 +25,22 @@ def is_after_24h(time):
     now = django_tz.now()
     timedelta_per_second = (now-time).total_seconds()
     if timedelta_per_second/(3600*24) > 1:
+        return True
+    return False
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def is_in_iran(ip):
+    client = Client(589085, 's9lO8TbLzvP03RjD', host='geolite.info')
+    country = client.country(ip)
+    if country.country.iso_code == 'IR':
         return True
     return False
